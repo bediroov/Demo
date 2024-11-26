1b9e4563a4dc9385934297a648b1e3016237205c
nihadamirov <nihadamirov98@gmail.com>
2024-11-26 18:09:01 +0400

    init project

---
+++ src/main/java/az/innakhchivan/InNakhchivanBackendApplication.java
@@ -0,0 +1,13 @@
+package az.innakhchivan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InNakhchivanBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InNakhchivanBackendApplication.class, args);
+    }
+
+}
