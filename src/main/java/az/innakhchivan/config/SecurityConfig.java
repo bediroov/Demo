1daa9aaa0d5c8b0fa396ab34fd4f2ca8b1228c0d
bediroov <hafizbedirov99@gmail.com>
2025-03-01 12:27:41 +0400

    lastt

--- src/main/java/az/innakhchivan/config/SecurityConfig.java
+++ src/main/java/az/innakhchivan/config/SecurityConfig.java
@@ -1,10 +1,14 @@
 package az.innakhchivan.config;
+
+import az.innakhchivan.enums.Role;
 import az.innakhchivan.exception.CustomAccessDeniedFilter;
 import az.innakhchivan.security.JwtAuthFilter;
 import az.innakhchivan.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,7 +21,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 
-
+@Slf4j
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
@@ -31,37 +35,44 @@ public class SecurityConfig {
     private final CustomAccessDeniedFilter customAccessDeniedFilter;
 
 
-    private static final String[] AUTH_WHITELIST = {
+    private static final String[] SWAGGER_WHITELIST = {
             "/v3/api-docs/**",
             "/swagger-ui/**",
-            "/api/v1/**"
     };
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        log.info("Configuring SecurityFilterChain...");
+
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req -> req
-                        .requestMatchers(AUTH_WHITELIST).permitAll()
-                        .requestMatchers("/api/v1/auth/register", "/api/v1/auth/login", "/api/v1/auth/logout").permitAll()
-                        .requestMatchers("/api/v1/auth/**").hasAnyRole("USER", "ADMIN")
-                        .requestMatchers("/api/v1/about/all/**",
-                        "/api/v1/becoming-an-entrepreneur-in-nakhinvest/all/**",
-                        "/api/v1/contact/all/**",
-                        "/api/v1/incentive/all/**",
-                        "/api/v1/news/all?**",
-                        "/api/v1/partner-feedback/all/**",
-                        "/api/v1/image/all/**",
-                        "/api/v1/image/download-by-url/**",
-                        "/api/v1/project/all/**",
-                        "/api/v1/question/all/**",
-                        "/api/v1/sector/all/**",
-                        "/api/v1/video-gallery/all/**",
-                        "/api/v1/why-nakhinvest/all/**",
-                        "/api/v1/write-to-us/all/**").hasAuthority("USER")
-                        .requestMatchers("/api/v1/**").hasAuthority("USER")
-                        .requestMatchers("/api/v1/**").hasAuthority("ADMIN")
-                        .anyRequest().authenticated()
+                                .requestMatchers(SWAGGER_WHITELIST).permitAll()
+//
+                                .requestMatchers("/api/v1/auth/register").permitAll()
+                                .requestMatchers("/api/v1/auth/login").permitAll()
+                                .requestMatchers("/api/v1/auth/logout").permitAll()
+                                //
+                                .requestMatchers("/api/v1/map-data").hasAnyAuthority(Role.ROLE_ADMIN.getAuthority())
+                                //
+                                .requestMatchers("/api/v1/auth/**").hasAnyRole("USER", "ADMIN")
+                                .requestMatchers("/api/v1/about/all/**",
+                                        "/api/v1/becoming-an-entrepreneur-in-nakhinvest/all/**",
+                                        "/api/v1/contact/all/**",
+                                        "/api/v1/incentive/all/**",
+                                        "/api/v1/news/all?**",
+                                        "/api/v1/partner-feedback/all/**",
+                                        "/api/v1/image/all/**",
+                                        "/api/v1/image/download-by-url/**",
+                                        "/api/v1/project/all/**",
+                                        "/api/v1/question/all/**",
+                                        "/api/v1/sector/all/**",
+                                        "/api/v1/video-gallery/all/**",
+                                        "/api/v1/why-nakhinvest/all/**",
+                                        "/api/v1/write-to-us/all/**").hasAuthority("USER")
+                                .requestMatchers("/api/v1/**").hasAuthority("USER")
+                                .requestMatchers("/api/v1/**").hasAuthority("ADMIN")
+                                .anyRequest().authenticated()
                 )
                 .exceptionHandling(e ->
                         e.accessDeniedHandler(customAccessDeniedFilter)
--- src/main/java/az/innakhchivan/dto/request/LoginRequestDto.java
+++ src/main/java/az/innakhchivan/dto/request/LoginRequestDto.java
@@ -1,11 +1,15 @@
 package az.innakhchivan.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
 public class LoginRequestDto {
     @NotBlank(message = "Username is required")
     private String username;
