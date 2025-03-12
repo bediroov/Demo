367c11371eb5a7badd8472c1b281e90f8d0deb21
bediroov <hafizbedirov99@gmail.com>
2025-03-12 22:42:51 +0400

    ks

--- src/main/java/az/innakhchivan/InNakhchivanBackendApplication.java
+++ src/main/java/az/innakhchivan/InNakhchivanBackendApplication.java
@@ -1,5 +1,4 @@
 package az.innakhchivan;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.retry.annotation.EnableRetry;
--- src/main/java/az/innakhchivan/config/SecurityConfig.java
+++ src/main/java/az/innakhchivan/config/SecurityConfig.java
@@ -46,6 +46,7 @@ public class SecurityConfig {
                         .requestMatchers(AUTH_WHITELIST).permitAll()
                         .requestMatchers("/api/v1/auth/register", "/api/v1/auth/login", "/api/v1/auth/logout").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/v1/contact", "/api/v1/submit-project", "/api/v1/pdf/upload").permitAll()
+
                         .requestMatchers(HttpMethod.GET,
                                 "/api/v1/search",
                                 "/api/v1/why-nakhinvest",
